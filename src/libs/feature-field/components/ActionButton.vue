5ddc84af27fb8bf600771fe20d9b4822a13c60e0
YutaK1026 <89566282+YutaK1026@users.noreply.github.com>
2022-08-21 08:39:22 +0900

    add-augmented-uiが動くようにした

--- src/libs/feature-field/components/ActionButton.vue
+++ src/libs/feature-field/components/ActionButton.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-btn @click="$emit('handleAction')" class="btn action" v-if="isEnableAction"
-    >発動</v-btn
-  >
-  <p v-else>発動可能なカードを選択してください</p>
+  <div data-augmented-ui="tl-round-inset both" class="styleme">
+      <link rel="stylesheet" type="text/css" href="https://unpkg.com/augmented-ui@2/augmented-ui.min.css">
+      <v-btn @click="$emit('handleAction')" class="btn action" v-if="isEnableAction"
+        >発動</v-btn>
+      <p v-else>発動可能なカードを選択してください</p>
+    </div>
 </template>
 
 <script>
