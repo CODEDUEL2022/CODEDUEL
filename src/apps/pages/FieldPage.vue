38a43673b0a0238e46b6924b8bed2034480fd3bb
yutteee <yusaku_nakamura_aa@icloud.com>
2022-08-20 14:45:35 +0900

    fieldPageのコンポーネントを分けた #17

---
+++ src/apps/pages/FieldPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <FieldTemplate />
+</template>
+<script>
+import FieldTemplate from "/src/libs/feature-field/templates/field-template.vue";
+
+export default {
+  name: "field",
+  components: {
+    FieldTemplate,
+  },
+};
+</script>
+<style scoped></style>
---
+++ src/libs/feature-field/components/ActionButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-btn @click="useCards" class="btn action" v-if="attack_decision">発動</v-btn>
+    <p v-else>発動可能なカードを選択してください</p>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.btn {
+  margin-right: 1rem;
+  width: 100px;
+}
+
+.btn.action {
+  color: #102335;
+  border: 1px solid #d3fffd;
+  box-shadow: 0px 0px 20px #d3fffd;
+}
+</style>
