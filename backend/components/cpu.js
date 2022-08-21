1bb4e3fa0818668ae603bd28f52758efb0e9daee
YutaK1026 <89566282+YutaK1026@users.noreply.github.com>
2022-08-22 00:02:22 +0900

    add-cpuに関する関数を実装-途中経過

--- backend/components/cpu.js
+++ backend/components/cpu.js
@@ -1,22 +1,28 @@
-import playerDB from "DB.js";
 import cardDB from "DB.js";
 import comboDB from "DB.js";
 
-export const initialize = function (req, res) {
-  playerDB.push({
-    RoomId: req.body.RoomId,
-    playerId: req.body.PlayerId,
-    cardList: [],
-    myHP: 200,
-    enemyHP: 200,
-    cardListNumber: [],
-    trunFlag: 0,
-  });
-};
+let cpuHP = 0
+let cpuComboFrec = 0
 
-export const cpuAttack = function (req, res) {
-  let flag = Math.floor(Math.random() * 3);
-  if (flag % 2 == 0) {
+//levelはフロントエンドから貰う。CPUの強さを指定している
+export const cpuLevel = function(req, res){
+  if(req.body.cpuLevel == 0){
+    cpuHP = 100
+    cpuComboFrec = 10
+  }else if(req.body.cpuLevel == 1){
+    cpuHP = 200
+    cpuComboFrec = 20
+  }else if(req.body.cpuLevel == 2){
+    cpuHP = 300
+    cpuComboFrec = 30
+  }
+}
+
+//CPUの攻撃を決める
+export const cpuAttack = function () {
+  let flag = Math.floor(Math.random() * 10);
+  if (flag < cpuComboFrec) {
+    //フロントエンドから受け取るreqの形を想定して作成
     const tmp = Number(Math.floor(Math.random() * cardDB.length));
     return cardDB[tmp];
   } else {
@@ -24,3 +30,9 @@ export const cpuAttack = function (req, res) {
     return comboDB[tmp];
   }
 };
+
+//CPUのDBを作ってる。フロントエンドに渡すよう
+export const CPUDB = [{
+  myHP: cpuHP,
+  turnFlag: 0
+}];
