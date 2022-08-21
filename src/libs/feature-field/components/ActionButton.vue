5cde9ff6338152bfe363879a4ba5e1088abe50d2
YAMADA Riko <lycopene109@outlook.jp>
2022-08-21 10:19:34 +0900

    ADD：一時的に

--- src/libs/feature-field/components/ActionButton.vue
+++ src/libs/feature-field/components/ActionButton.vue
@@ -1,10 +1,41 @@
 <template>
-  <div data-augmented-ui="tl-round-inset both" class="styleme">
-      <link rel="stylesheet" type="text/css" href="https://unpkg.com/augmented-ui@2/augmented-ui.min.css">
-      <v-btn @click="$emit('handleAction')" class="btn action" v-if="isEnableAction"
-        >発動</v-btn>
+  <div class="container">
+    <div class="wrapper">
+      <div class="bg-square"></div>
+      <div
+        data-augmented-ui="tl-2-rect tr-2-rect bl-2-rect br-2-rect none"
+        class="btn shadow"
+        @click="$emit('handleAction')"
+        v-if="isEnableAction"
+      >
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://unpkg.com/augmented-ui@2/augmented-ui.min.css"
+        />
+        <span>発動</span>
+      </div>
       <p v-else>発動可能なカードを選択してください</p>
     </div>
+
+    <div class="wrapper">
+      <div class="bg-square"></div>
+      <div
+        data-augmented-ui="tl-2-rect tr-2-rect bl-2-rect br-2-rect none"
+        class="btn shadow"
+        @click="$emit('handleAction')"
+        v-if="isEnableAction"
+      >
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://unpkg.com/augmented-ui@2/augmented-ui.min.css"
+        />
+        <span>発動</span>
+      </div>
+      <p v-else>発動可能なカードを選択してください</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,14 +46,33 @@ export default {
 </script>
 
 <style scoped>
-.btn {
-  margin: 2rem;
-  width: 100px;
+.container {
+  display: flex;
+}
+.wrapper {
+  bottom: 1rem;
+  right: 1rem;
+}
+.bd-square {
+  box-shadow: 0px 0px 50px #d3fffd;
 }
 
-.btn.action {
-  color: #102335;
-  border: 1px solid #d3fffd;
-  box-shadow: 0px 0px 20px #d3fffd;
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+  width: 10em;
+  height: 10em;
+  background-color: #144f61;
+}
+
+.btn:hover {
+  background-color: #41bfb9;
+}
+
+.btn span {
+  z-index: 1;
+  color: #d3fffd;
 }
 </style>
