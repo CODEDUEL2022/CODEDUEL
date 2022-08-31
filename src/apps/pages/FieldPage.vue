ed4abaef2c39a35fcb6c89372311e0534210d8fc
YAMADA Riko <lycopene109@outlook.jp>
2022-09-01 01:11:27 +0900

    プロパティ周りの修正

--- src/apps/pages/FieldPage.vue
+++ src/apps/pages/FieldPage.vue
@@ -1,5 +1,6 @@
 <template>
   <FieldTemplate
+    :cardsList="cardsList"
     :message="message"
     :showGeneralCutIn="showGeneralCutIn"
     :showActionCutIn="showActionCutIn"
@@ -23,186 +24,192 @@
   />
 </template>
 <script>
-import FieldTemplate from "/src/libs/feature-field/templates/field-template.vue";
+  import FieldTemplate from "/src/libs/feature-field/templates/field-template.vue";
 
-export default {
-  name: "field",
-  components: {
-    FieldTemplate,
-  },
-  data() {
-    return {
-      message: "相手が入室するまでしばらくお待ちください",
-      showGeneralCutIn: true,
-      showActionCutIn: false,
-      action: "attack",
-      value: 20,
-      yourHP: 150,
-      opponentHP: 180,
-      roundCount: 1,
-      yourCardsData: [],
-      selectedCardsData: [],
-      yourId: "",
-      roomId: "",
-      yourImg: "",
-      selectedId: "",
-      selectedImg: "",
-      attackSignal: 0,
-      comboData: [],
-      opponentTrun: false,
-      isAlone: false,
-      usedCardIdList: [], //攻撃された、攻撃したカードのIDのリスト
-    };
-  },
-  created() {
-    this.yourCardsData = [];
-    const searchParams = new URLSearchParams(window.location.search);
-    console.log(this.yourCardsData);
-    this.$axios.get("/getComboDb").then((res) => {
-      for (let i = 0; i < res.data.length; i++) {
-        this.comboData.push(res.data[i]);
-      }
-    });
-    //HPの共有
-    this.$axios
-      .post("/HPReload", {
-        playerId: searchParams.get("id"),
-      })
-      .then((res) => {
-        console.log(res.data);
-        this.yourHP = res.data.yourHP;
-        this.opponentHP = res.data.opponentHP;
-      });
-    // カードをドローする処理
-    this.$axios
-      .post("/cardDraw", {
-        cardData: this.yourCardsData,
-        playerId: searchParams.get("id"),
-      })
-      .then((res) => {
-        console.log(res.data);
-        for (let i = 0; i < res.data.length; i++) {
-          this.yourCardsData.push(res.data[i]);
-        }
-        console.log(this.yourCardsData);
-        console.log("hogehoge");
-      });
-
-    //joinするための送信
-    this.yourId = searchParams.get("id");
-    this.roomID = searchParams.get("room");
-    this.socket.emit("roomJoin", this.roomID, this.yourId);
-    //turn_flagに応じて、showAttackなどの表示、非表示を決定する。
-    //偶数の時は自分の番
-    this.$axios
-      .post("/getTurn", { playerId: searchParams.get("id") })
-      .then((res) => {
-        if (res.data % 2 == 0) {
-          this.oponentTurn = false;
-        } else if (res.data == 1) {
-          this.oponentTurn = true;
-          this.message = "相手が入室するまでしばらくお待ちください";
-        } else {
-          this.oponentTurn = true;
-          this.message = "相手のターンです";
-        }
-      });
-  },
-  methods: {
-    // 可能なコンボを取得
-    attackOptions: function () {
-      let updatedData = this.selectedCardsData.map((obj) => obj.id);
-      let ableAttackData = updatedData.sort((a, b) => (a < b ? -1 : 1));
-      // 一致してるものがあるかを判定
-      const isIncludes = (arr, target) =>
-        arr.every((el) => target.includes(el));
-      if (ableAttackData.length === 0) {
-        return [];
-      } else {
-        // updateddataにあるのと一致した攻撃だけを返す
-        return this.comboData.filter((comboData) => {
-          return isIncludes(ableAttackData, comboData.idList);
-        });
-      }
+  export default {
+    name: "field",
+    components: {
+      FieldTemplate,
     },
-    //発動できるかどうかを判定する
-    isEnableAction: function () {
-      let updatedData = this.selectedCardsData.map((obj) => obj.id);
-      updatedData.sort(function (first, second) {
-        if (first > second) {
-          return 1;
-        } else if (first < second) {
-          return -1;
-        } else {
-          return 0;
+    data() {
+      return {
+        cardsList: [],
+        message: "相手が入室するまでしばらくお待ちください",
+        showGeneralCutIn: true,
+        showActionCutIn: false,
+        action: "attack",
+        value: 20,
+        yourName: "User1",
+        yourHP: 200,
+        opponentName: "User2",
+        opponentHP: 200,
+        roundCount: 1,
+        yourCardsData: [],
+        selectedCardsData: [],
+        yourId: "",
+        roomId: "",
+        yourImg: "",
+        selectedId: "",
+        selectedImg: "",
+        attackSignal: 0,
+        comboData: [],
+        opponentTrun: false,
+        isAlone: false,
+        usedCardIdList: [], //攻撃された、攻撃したカードのIDのリスト
+      };
+    },
+    created() {
+      this.yourCardsData = [];
+      const searchParams = new URLSearchParams(window.location.search);
+      console.log(this.yourCardsData);
+      this.$axios.get("/getComboDb").then((res) => {
+        for (let i = 0; i < res.data.length; i++) {
+          this.comboData.push(res.data[i]);
         }
       });
-      // 一致してるものがあるかを判定
-      const isIncludes = (arr, target) =>
-        arr.every((el) => target.includes(el));
-  
-      if (updatedData.length === 0) {
-        return false;
-      } else if (updatedData.length === 1) {
-        // this.cardValue.value = this.selectedCardsData[0].value;
-        return true;
-      } else {
-        let ableCombo = this.comboData.filter((comboData) => {
-          return isIncludes(updatedData, comboData.idList);
+      //HPの共有
+      this.$axios
+        .post("/HPReload", {
+          playerId: searchParams.get("id"),
+        })
+        .then((res) => {
+          console.log(res.data);
+          this.yourHP = res.data.yourHP;
+          this.opponentHP = res.data.opponentHP;
         });
-        // 完全一致した攻撃だけを返す
-        for (let i = 0, n = updatedData.length; i < n; ++i) {
-        if (ableCombo.length == 0) {
+      // カードをドローする処理
+      this.$axios
+        .post("/cardDraw", {
+          cardData: this.yourCardsData,
+          playerId: searchParams.get("id"),
+        })
+        .then((res) => {
+          console.log(res.data);
+          for (let i = 0; i < res.data.length; i++) {
+            this.yourCardsData.push(res.data[i]);
+          }
+          console.log(this.yourCardsData);
+          console.log("hogehoge");
+        });
+
+      //joinするための送信
+      this.yourId = searchParams.get("id");
+      this.roomID = searchParams.get("room");
+      this.socket.emit("roomJoin", this.roomID, this.yourId);
+      //turn_flagに応じて、showAttackなどの表示、非表示を決定する。
+      //偶数の時は自分の番
+      this.$axios
+        .post("/getTurn", { playerId: searchParams.get("id") })
+        .then((res) => {
+          if (res.data % 2 == 0) {
+            this.oponentTurn = false;
+          } else if (res.data == 1) {
+            this.oponentTurn = true;
+            this.message = "相手が入室するまでしばらくお待ちください";
+          } else {
+            this.oponentTurn = true;
+            this.message = "相手のターンです";
+          }
+        });
+    },
+    methods: {
+      // 可能なコンボを取得
+      attackOptions: function () {
+        let updatedData = this.selectedCardsData.map((obj) => obj.id);
+        let ableAttackData = updatedData.sort((a, b) => (a < b ? -1 : 1));
+        // 一致してるものがあるかを判定
+        const isIncludes = (arr, target) =>
+          arr.every((el) => target.includes(el));
+        if (ableAttackData.length === 0) {
+          return [];
+        } else {
+          // updateddataにあるのと一致した攻撃だけを返す
+          return this.comboData.filter((comboData) => {
+            return isIncludes(ableAttackData, comboData.idList);
+          });
+        }
+      },
+      //発動できるかどうかを判定する
+      isEnableAction: function () {
+        let updatedData = this.selectedCardsData.map((obj) => obj.id);
+        updatedData.sort(function (first, second) {
+          if (first > second) {
+            return 1;
+          } else if (first < second) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+        // 一致してるものがあるかを判定
+        const isIncludes = (arr, target) =>
+          arr.every((el) => target.includes(el));
+
+        if (updatedData.length === 0) {
           return false;
-        } else if (
-          updatedData[i] == ableCombo[0].idList[i] &&
-          updatedData.length == ableCombo[0].idList.length
-        ) {
+        } else if (updatedData.length === 1) {
+          // this.cardValue.value = this.selectedCardsData[0].value;
           return true;
         } else {
-          return false;
+          let ableCombo = this.comboData.filter((comboData) => {
+            return isIncludes(updatedData, comboData.idList);
+          });
+          // 完全一致した攻撃だけを返す
+          for (let i = 0, n = updatedData.length; i < n; ++i) {
+            if (ableCombo.length == 0) {
+              return false;
+            } else if (
+              updatedData[i] == ableCombo[0].idList[i] &&
+              updatedData.length == ableCombo[0].idList.length
+            ) {
+              return true;
+            } else {
+              return false;
+            }
+          }
         }
+      },
+      closeGeneralCutIn: function () {
+        this.showGeneralCutIn = false;
+      },
+      closeActionCutIn: function () {
+        this.showActionCutIn = false;
+        this.roundCount += 1;
+      },
+      handleAction: function () {
+        this.showActionCutIn = true;
+        this.selectedCardsData.splice(
+          this.index,
+          this.selectedCardsData.length
+        );
+      },
+    },
+    mounted() {
+      let anotherThis = this;
+      this.socket.on("numPlayer", function (numPlayer) {
+        if (numPlayer == 1) {
+          anotherThis.isAlone = true;
+        } else {
+          anotherThis.isAlone = false;
         }
-      }
-    },
-    closeGeneralCutIn: function () {
-      this.showGeneralCutIn = false;
-    },
-    closeActionCutIn: function () {
-      this.showActionCutIn = false;
-      this.roundCount += 1;
-    },
-    handleAction: function () {
-      this.showActionCutIn = true;
-      this.selectedCardsData.splice(this.index, this.selectedCardsData.length);
-    },
-  },
-  mounted() {
-    let anotherThis = this;
-    this.socket.on("numPlayer", function (numPlayer) {
-      if (numPlayer == 1) {
-        anotherThis.isAlone = true;
-      } else {
-        anotherThis.isAlone = false;
-      }
-    });
+      });
 
-    this.socket.on("HPinfo", function (HPinfo) {
-      anotherThis.action = HPinfo.action; //攻撃の種類
-      anotherThis.usedCardIdList = HPinfo.usedCardIdList; //カードのIDのリスト
-      if (HPinfo.attackedPlayerID == anotherThis.playerId) {
-        //攻撃した時の処理
-        anotherThis.yourHP = HPinfo.attackedPlayerHP;
-        anotherThis.opponentHP = HPinfo.damagedPlayerHP;
-        anotherThis.opponentTrun = true;
-      } else if (HPinfo.damagedPlayerID == anotherThis.playerId) {
-        //攻撃された時の処理
-        anotherThis.yourHP = HPinfo.damagedPlayerHP;
-        anotherThis.opponentHP = HPinfo.attackedPlayerHP;
-        anotherThis.opponentTrun = false;
-      }
-    });
-  },
-};
+      this.socket.on("HPinfo", function (HPinfo) {
+        anotherThis.action = HPinfo.action; //攻撃の種類
+        anotherThis.usedCardIdList = HPinfo.usedCardIdList; //カードのIDのリスト
+        if (HPinfo.attackedPlayerID == anotherThis.playerId) {
+          //攻撃した時の処理
+          anotherThis.yourHP = HPinfo.attackedPlayerHP;
+          anotherThis.opponentHP = HPinfo.damagedPlayerHP;
+          anotherThis.opponentTrun = true;
+        } else if (HPinfo.damagedPlayerID == anotherThis.playerId) {
+          //攻撃された時の処理
+          anotherThis.yourHP = HPinfo.damagedPlayerHP;
+          anotherThis.opponentHP = HPinfo.attackedPlayerHP;
+          anotherThis.opponentTrun = false;
+        }
+      });
+    },
+  };
 </script>
 <style scoped></style>
--- src/libs/feature-field/templates/field-template.vue
+++ src/libs/feature-field/templates/field-template.vue
@@ -15,7 +15,12 @@
         @closeActionCutIn="$emit('closeActionCutIn')"
       />
     </div>
-    <HPDisplay :yourHP="yourHP" :opponentHP="opponentHP"></HPDisplay>
+    <HPDisplay
+      :yourName="yourName"
+      :yourHP="yourHP"
+      :opponentName="opponentName"
+      :opponentHP="opponentHP"
+    ></HPDisplay>
     <RoundDisplay :roundCount="roundCount"></RoundDisplay>
     <v-row>
       <v-col cols="3">
@@ -35,7 +40,9 @@
             <SimpleCard
               v-for="card in selectedCardsData"
               :focusedCard="card"
-              :key="`${Math.random().toString(32).substring(2)}-first-${card.id}`"
+              :key="`${Math.random().toString(32).substring(2)}-first-${
+                card.id
+              }`"
             ></SimpleCard>
           </VueDrag>
         </div>
@@ -66,73 +73,76 @@
 </template>
 
 <script>
-import ActionButton from "../components/ActionButton.vue";
-import HPDisplay from "../components/HpDisplay.vue";
-import RoundDisplay from "../components/RoundDisplay.vue";
-import VueDrag from "vuedraggable";
-import GeneralCutIn from "../components/GeneralCutIn.vue";
-import ActionCutIn from "../components/ActionCutIn.vue";
-import TerminalUI from "../components/TerminalUI.vue";
-import SimpleCard from "../components/SimpleCard.vue";
+  import ActionButton from "../components/ActionButton.vue";
+  import HPDisplay from "../components/HpDisplay.vue";
+  import RoundDisplay from "../components/RoundDisplay.vue";
+  import VueDrag from "vuedraggable";
+  import GeneralCutIn from "../components/GeneralCutIn.vue";
+  import ActionCutIn from "../components/ActionCutIn.vue";
+  import TerminalUI from "../components/TerminalUI.vue";
+  import SimpleCard from "../components/SimpleCard.vue";
 
-export default {
-  name: "FieldTemplate",
-  components: {
-    ActionButton,
-    HPDisplay,
-    RoundDisplay,
-    VueDrag,
-    GeneralCutIn,
-    ActionCutIn,
-    TerminalUI,
-    SimpleCard,
-  },
-  props: [
-    "message",
-    "showGeneralCutIn",
-    "showActionCutIn",
-    "action",
-    "value",
-    "yourHP",
-    "opponentHP",
-    "roundCount",
-    "yourCardsData",
-    "selectedCardsData",
-    "yourGroup",
-    "yourId",
-    "yourImg",
-    "selectedId",
-    "selectedImg",
-    "comboData",
-    "isEnableAction",
-    "attackOptions",
-    "focusedCard",
-  ],
-  data() {
-    return {
-      // drag&drop用のデータ
-      options: {
-        group: "yourGroup",
-        animation: 200,
-      },
-    };
-  },
-};
+  export default {
+    name: "FieldTemplate",
+    components: {
+      ActionButton,
+      HPDisplay,
+      RoundDisplay,
+      VueDrag,
+      GeneralCutIn,
+      ActionCutIn,
+      TerminalUI,
+      SimpleCard,
+    },
+    props: [
+      "cardsList",
+      "message",
+      "showGeneralCutIn",
+      "showActionCutIn",
+      "action",
+      "value",
+      "yourName",
+      "yourHP",
+      "opponentName",
+      "opponentHP",
+      "roundCount",
+      "yourCardsData",
+      "selectedCardsData",
+      "yourGroup",
+      "yourId",
+      "yourImg",
+      "selectedId",
+      "selectedImg",
+      "comboData",
+      "isEnableAction",
+      "attackOptions",
+      "focusedCard",
+    ],
+    data() {
+      return {
+        // drag&drop用のデータ
+        options: {
+          group: "yourGroup",
+          animation: 200,
+        },
+      };
+    },
+  };
 </script>
 
 <style scoped>
-.field {
-  height: 300px;
-  width: 100%;
-  background: rgba(211, 255, 253);
-  border: 2px solid #d3fffd;
-  box-shadow: 0px 0px 50px #d3fffd;
-}
+  .field {
+    height: 300px;
+    width: 100%;
+    background: rgba(211, 255, 253);
+    border: 2px solid #d3fffd;
+    box-shadow: 0px 0px 50px #d3fffd;
+  }
 
-.area {
-  display: flex;
-  justify-content: stretch;
-  width: 1500px;
-  height: 300px;
-}
+  .area {
+    display: flex;
+    justify-content: stretch;
+    width: 1500px;
+    height: 300px;
+  }
 </style>
