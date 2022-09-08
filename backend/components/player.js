aa03ba36a30d8ab8d4612d608338ca2572411e17
maron <riku20020801@gmail.com>
2022-09-08 23:37:32 +0900

    FIX:ERROR

--- backend/components/player.js
+++ backend/components/player.js
@@ -146,6 +146,22 @@ export const controlTurn = function (req, res) {
   playerDB[selectId].turnFlag += 1;
   playerDB[selectTurnId].turnFlag += 1;
 };
+export const findOpponentUser = function (playerId) {
+  const selectTurnId = playerDB.findIndex((e) => e.playerId === playerId);
+  const thisRoomId = playerDB[selectTurnId].RoomId;
+  try {
+    playerDB.filter((e) => {
+      if (e.RoomId === thisRoomId && e.playerId != playerId) {
+        return true;
+      }
+    })[0].playerId;
+  } catch (e) {
+    console.log("まだ相手プレイヤーがいません");
+    return false;
+  }
+  console.log("相手プレイヤーがいます！");
+  return true;
+};
 
 export const reload = function (req, res) {
   let playerDBFromLocalStorage = {
@@ -176,161 +192,89 @@ const decideUsedCombo = function (selectedData) {
       return comboDB[i];
   }
 };
- export const getTurn = function (req, res) {
-   const selectTurnId = playerDB.findIndex(
-     (e) => e.playerId === req.body.playerId
-   );
-   return playerDB[selectTurnId].turnFlag;
- };
- 
- export const controlTurn = function (req, res) {
-   const selectTurnId = playerDB.findIndex(
-     (e) => e.playerId === req.body.playerId
-   );
-   const thisRoomId = playerDB[selectTurnId].RoomId;
-   const thisRoomPlayer = playerDB.filter((e) => {
-     if (e.RoomId === thisRoomId && e.playerId != req.body.playerId) {
-       return true;
-     }
-   });
- 
-   const selectId = playerDB.findIndex(
-     (e) => e.playerId === thisRoomPlayer[0].playerId
-   );
-   playerDB[selectId].turnFlag += 1;
-   playerDB[selectTurnId].turnFlag += 1;
- };
- export const findOpponentUser = function(playerId){
-  const selectTurnId = playerDB.findIndex(
-    (e) => e.playerId === playerId
+
+export const calculateHP = function (cardValue, playerId) {
+  const indexAttacked = playerDB.findIndex((e) => e.playerId === playerId);
+  const thisRoomPlayer = playerDB.filter((e) => {
+    if (e.RoomId === cardValue.roomId && e.playerId != playerId) {
+      return true;
+    }
+  });
+  const indexDamaged = playerDB.findIndex(
+    (e) => e.playerId === thisRoomPlayer[0].playerId
   );
-  const thisRoomId = playerDB[selectTurnId].RoomId;
-  try{
-    playerDB.filter((e) => {
-      if (e.RoomId === thisRoomId && e.playerId != playerId) {
-        return true;
-      }
-    })[0].playerId
-  }catch (e){
-    console.log("まだ相手プレイヤーがいません")
-    return false
+  let effect = "";
+  let damageValue = 0;
+  let updatedData = cardValue.selectedCardData.map((obj) => obj.id);
+  updatedData.sort(function (first, second) {
+    if (first > second) {
+      return 1;
+    } else if (first < second) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  let thisTurnField = playerDB[indexAttacked].field;
+  changeField(indexAttacked);
+  changeField(indexDamaged);
+  let nextTurnField = playerDB[indexAttacked].field;
+  let fieldBonus = 0;
+  let fieldBonusFlag = "false";
+  changeRound(indexAttacked);
+  changeRound(indexDamaged);
+  let nextRoundCount = playerDB[indexAttacked].roundCount;
+  if (cardValue.selectedCardData.length == 1) {
+    if (cardValue.selectedCardData[0].field == fieldDB[thisTurnField]) {
+      fieldBonus = 10;
+      fieldBonusFlag = "true";
+    }
+    damageValue = cardValue.selectedCardData[0].actionValue + fieldBonus;
+    if (cardValue.selectedCardData[0].action == "enhancement") {
+      //回復の処理
+      effect += "enhancement";
+      playerDB[indexAttacked].yourHP += damageValue;
+      playerDB[indexDamaged].opponentHP += damageValue;
+    } else if (cardValue.selectedCardData[0].action == "steal") {
+      //ハッカーカードの処理
+      effect += "steal";
+      playerDB[indexAttacked].yourHP += damageValue;
+      playerDB[indexDamaged].opponentHP += damageValue;
+      playerDB[indexAttacked].opponentHP -= damageValue;
+      playerDB[indexDamaged].yourHP -= damageValue;
+    } else {
+      //攻撃の処理
+      effect += "attack";
+      playerDB[indexAttacked].opponentHP -= damageValue;
+      playerDB[indexDamaged].yourHP -= damageValue;
+    }
+  } else {
+    // コンボの場合
+    const usedCombo = decideUsedCombo(updatedData);
+    effect += usedCombo.nameEn;
+    damageValue = usedCombo.actionValue;
+    playerDB[indexAttacked].opponentHP -= usedCombo.actionValue;
+    playerDB[indexDamaged].yourHP -= usedCombo.actionValue;
   }
-  console.log("相手プレイヤーがいます！")
-  return true
- }
- 
- export const reload = function (req, res) {
-   let playerDBFromLocalStorage = {
-     RoomId: localStorage.getItem("RoomId"),
-     playerId: localStorage.getItem("playerId"),
-     cardList: localStorage.getItem("cardList"),
-     yourHP: localStorage.getItem("yourHP"),
-     opponentHP: localStorage.getItem("opponentHP"),
-     cardListNumber: localStorage.getItem("cardListNumber"),
-     turnFlag: localStorage.getItem("turnFlag"),
-   };
-   return playerDBFromLocalStorage;
- };
- 
- const decideUsedCombo = function (selectedData) {
-   const isEqualArray = function (array1, array2) {
-     if (array1.length != array2.length) return false;
-     for (let i = 0; i < array1.length; i++) {
-       if (array1[i] !== array2[i]) return false;
-     }
-     return true;
-   };
-   for (let i = 0; i < comboDB.length; i++) {
-     if (
-       isEqualArray(selectedData, comboDB[i].idList) &&
-       selectedData.length == comboDB[i].idList.length
-     )
-       return comboDB[i];
-   }
- };
- 
- export const calculateHP = function (cardValue, playerId) {
-   const indexAttacked = playerDB.findIndex((e) => e.playerId === playerId);
-   const thisRoomPlayer = playerDB.filter((e) => {
-     if (e.RoomId === cardValue.roomId && e.playerId != playerId) {
-       return true;
-     }
-   });
-   const indexDamaged = playerDB.findIndex(
-     (e) => e.playerId === thisRoomPlayer[0].playerId
-   );
-   let effect = "";
-   let damageValue = 0;
-   let updatedData = cardValue.selectedCardData.map((obj) => obj.id);
-   updatedData.sort(function (first, second) {
-     if (first > second) {
-       return 1;
-     } else if (first < second) {
-       return -1;
-     } else {
-       return 0;
-     }
-   });
-   let thisTurnField = playerDB[indexAttacked].field;
-   changeField(indexAttacked);
-   changeField(indexDamaged);
-   let nextTurnField = playerDB[indexAttacked].field;
-   let fieldBonus = 0;
-   let fieldBonusFlag = "false";
-   changeRound(indexAttacked);
-   changeRound(indexDamaged);
-   let nextRoundCount = playerDB[indexAttacked].roundCount;
-   if (cardValue.selectedCardData.length == 1) {
-     if (cardValue.selectedCardData[0].field == fieldDB[thisTurnField]) {
-       fieldBonus = 10;
-       fieldBonusFlag = "true";
-     }
-     damageValue = cardValue.selectedCardData[0].actionValue + fieldBonus;
-     if (cardValue.selectedCardData[0].action == "enhancement") {
-       //回復の処理
-       effect += "enhancement";
-       playerDB[indexAttacked].yourHP += damageValue;
-       playerDB[indexDamaged].opponentHP += damageValue;
-     } else if (cardValue.selectedCardData[0].action == "steal") {
-       //ハッカーカードの処理
-       effect += "steal";
-       playerDB[indexAttacked].yourHP += damageValue;
-       playerDB[indexDamaged].opponentHP += damageValue;
-       playerDB[indexAttacked].opponentHP -= damageValue;
-       playerDB[indexDamaged].yourHP -= damageValue;
-     } else {
-       //攻撃の処理
-       effect += "attack";
-       playerDB[indexAttacked].opponentHP -= damageValue;
-       playerDB[indexDamaged].yourHP -= damageValue;
-     }
-   } else {
-     // コンボの場合
-     const usedCombo = decideUsedCombo(updatedData);
-     effect += usedCombo.nameEn;
-     damageValue = usedCombo.actionValue;
-     playerDB[indexAttacked].opponentHP -= usedCombo.actionValue;
-     playerDB[indexDamaged].yourHP -= usedCombo.actionValue;
-   }
-   const HPinfo = {
-     actionType: effect,
-     actionPoint: damageValue,
-     attackedPlayerID: playerDB[indexAttacked].playerId,
-     damagedPlayerID: playerDB[indexDamaged].playerId,
-     attackedPlayerHP: playerDB[indexAttacked].yourHP,
-     damagedPlayerHP: playerDB[indexDamaged].yourHP,
-     usedCardIdList: updatedData,
-     nextTurnField: nextTurnField,
-     fieldBonusFlag: fieldBonusFlag,
-     roundCount: nextRoundCount,
-   };
-   return HPinfo;
- };
- 
- const changeField = function (playerId) {
-   playerDB[playerId].field = (playerDB[playerId].field + 1) % 4;
- };
- 
- const changeRound = function (playerId) {
-   playerDB[playerId].roundCount = playerDB[playerId].roundCount + 1;
- };
+  const HPinfo = {
+    actionType: effect,
+    actionPoint: damageValue,
+    attackedPlayerID: playerDB[indexAttacked].playerId,
+    damagedPlayerID: playerDB[indexDamaged].playerId,
+    attackedPlayerHP: playerDB[indexAttacked].yourHP,
+    damagedPlayerHP: playerDB[indexDamaged].yourHP,
+    usedCardIdList: updatedData,
+    nextTurnField: nextTurnField,
+    fieldBonusFlag: fieldBonusFlag,
+    roundCount: nextRoundCount,
+  };
+  return HPinfo;
+};
+
+const changeField = function (playerId) {
+  playerDB[playerId].field = (playerDB[playerId].field + 1) % 4;
+};
+
+const changeRound = function (playerId) {
+  playerDB[playerId].roundCount = playerDB[playerId].roundCount + 1;
+};
