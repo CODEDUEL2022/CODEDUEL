d301a96826818f55cfa50e5f26060b450dd7d145
YAMADA Riko <lycopene109@outlook.jp>
2022-09-13 01:01:15 +0900

    FIX：途中経過

--- src/apps/pages/HomePage.vue
+++ src/apps/pages/HomePage.vue
@@ -15,8 +15,8 @@
   />
 </template>
 <script>
-import HomeTemplate from "/src/libs/feature-home/templates/home-template.vue";
-import io from "socket.io-client";
+  import HomeTemplate from "/src/libs/feature-home/templates/home-template.vue";
+  import io from "socket.io-client";
 
   export default {
     name: "home",
@@ -65,61 +65,59 @@ import io from "socket.io-client";
         });
       },
       onPushCPU: function () {
-        alert("Play with CPU. Happy hacking!");
+        this.startSE.play();
+        this.playerId = Math.random().toString(32).substring(2);
+        this.$axios
+          .post("/cpuPlayerData", {
+            playerId: this.playerId,
+            decId: 0, //仮においている。本来はデッキ選択用
+          })
+          .then((res) => {
+            //res.dataがRoomにいる人数ここで場合分けすればOK
+            console.log(res.data);
+          });
+        this.$router.push({
+          name: "field",
+          query: { room: this.roomId, id: this.playerId },
+        });
+      },
+      onPushCPU: function () {
         this.startSE.play();
         this.playerId = Math.random().toString(32).substring(2);
         this.$axios.post("/cpuPlayerData", {
           playerId: this.playerId,
           decId: 0, //仮においている。本来はデッキ選択用
-        })
-        .then((res) => {
-          //res.dataがRoomにいる人数ここで場合分けすればOK
-          console.log(res.data);
         });
-      this.$router.push({
-        name: "field",
-        query: { room: this.roomId, id: this.playerId },
-      });
+        this.$router.push({
+          name: "cpu",
+          query: { id: this.playerId },
+        });
+      },
+      onPushAutoMatching: function () {
+        this.startSE.play();
+        this.playerId = Math.random().toString(32).substring(2);
+        this.socket.emit("AutoMatchingPreLogin", this.playerId);
+        this.$router.push({
+          name: "waitingroom",
+          query: { id: this.playerId },
+        });
+      },
+      onStartModalOpen: function (user) {
+        this.startSE.play();
+        this.userName = user;
+        this.isStartModalOpen = true;
+      },
+      onStartModalClose: function () {
+        this.back1SE.play();
+        this.isStartModalOpen = false;
+      },
+      onShowHowToPlay: function () {
+        this.isHowToPlayOpen = true;
+      },
+      onHowToPlayClose: function () {
+        this.isHowToPlayOpen = false;
+      },
     },
-    onPushCPU: function () {
-      alert("Play with CPU. Happy hacking!");
-      this.startSE.play();
-      this.playerId = Math.random().toString(32).substring(2);
-      this.$axios.post("/cpuPlayerData", {
-        playerId: this.playerId,
-        decId: 0, //仮においている。本来はデッキ選択用
-      });
-      this.$router.push({
-        name: "cpu",
-        query: { id: this.playerId },
-      });
-    },
-    onPushAutoMatching: function () {
-      alert("Play random match. Happy hacking!");
-      this.startSE.play();
-      this.playerId = Math.random().toString(32).substring(2);
-      this.socket.emit("AutoMatchingPreLogin", this.playerId);
-      this.$router.push({
-        name: "waitingroom",
-        query: { id: this.playerId },
-      });
-    },
-    onStartModalOpen: function (user) {
-      this.startSE.play();
-      this.userName = user;
-      this.isStartModalOpen = true;
-    },
-    onStartModalClose: function () {
-      this.back1SE.play();
-      this.isStartModalOpen = false;
-    },
-    onShowHowToPlay: function () {
-      this.isHowToPlayOpen = true;
-    },
-    onHowToPlayClose: function () {
-      this.isHowToPlayOpen = false;
-    },
-  },
-};
+  };
 </script>
 <style scoped></style>
